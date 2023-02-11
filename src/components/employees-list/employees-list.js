396a66ad29dc2eedcca80abd91e7a2bec27f2c93
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-11 15:06:32 +0300

    Implemented dynamic loading of employee data in the EmployeeList component

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,17 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/>
+        );
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     );
 };
