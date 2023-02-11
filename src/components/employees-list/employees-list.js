9b9cd148bdb550f40401d308073b0ea8e87a5584
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-11 16:36:49 +0300

    Added key props to EmployeesList component

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -6,10 +6,10 @@ const EmployeesList = ({data}) => {
 
     const elements = data.map(item => {
         return (
-            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/>
+            <EmployeesListItem key={item.id} name={item.name} salary={item.salary} increase={item.increase}/>
         );
     });
-
+    
     return (
         <ul className="app-list list-group">
             {elements}
